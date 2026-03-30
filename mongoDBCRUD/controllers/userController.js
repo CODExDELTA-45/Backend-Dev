b7256cc9c70519aaf7927acb3aed3e03ba244ab5
Rohit Sharma <rohitsharma981818@gmail.com>
2026-03-30 15:11:53 +0530

    Rohit Sharma-mongoDBCRUD done

---
+++ mongoDBCRUD/controllers/userController.js
@@ -0,0 +1,16 @@
+import user from "../model/userSchema.js";
+
+export const getAllUsers = async(req,res)=>{
+    try{
+        const users = await user.find();
+        res.status(200).json({
+            success:true,
+            users
+        })
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:"Something went wrong"
+        })
+    }
+}
